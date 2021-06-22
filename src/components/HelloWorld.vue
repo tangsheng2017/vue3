1e35aa4955176cbc9374246cc7c2eea1d50723bf
tangsheng <316491034@qq.com>
2021-06-22 17:19:02 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <GMap :width="800" :height="400"></GMap>
+  </div>
+</template>
+
+<script>
+// import { defineAsyncComponent } from "vue";
+import GMap from "@/components/GMap"; //引入高德地图AGMap
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  components: {
+    GMap
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
